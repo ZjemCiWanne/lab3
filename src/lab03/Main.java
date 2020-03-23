7216fa1286b3a649337b846922dbb88ec83245b8
ZjemCiWanne <uiopmn@wp.pl>
2020-03-23 17:33:39 +0100

    Poprawione

--- src/lab03/Main.java
+++ src/lab03/Main.java
@@ -9,21 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         // Set<String> zbiorDni = new TreeSet<>();//treeset sortuje alfabetycznie
-        // zbiorDni.add("poniedziałek");
+        // zbiorDni.add("poniedziaĹ‚ek");
         // zbiorDni.add("wtorek");
         // zbiorDni.add("sroda");
         // zbiorDni.add("czwartek");
         // zbiorDni.add("piatek");
         // System.out.println(zbiorDni);
-    List<Student>studenci=new ArrayList<>();
-    studenci.add(new Student("Jan", "Kowalski"));
-    studenci.add(new Student("Janek", "Chybaty"));
-    studenci.add(new Student("Jan", "Kowalski"));
+        List<Student>studenci=new ArrayList<>();
+        studenci.add(new Student("Jan", "Kowalski"));
+        studenci.add(new Student("Janek", "Chybaty"));
+        studenci.add(new Student("Jan", "Kowalski"));
 
-    List<Student>studenci1=new ArrayList<>();
-    studenci1.add(new Student("Jakubowski", "Kowalski"));
-    studenci1.add(new Student("Janusz", "Chybaty"));
-    studenci1.add(new Student("Grazynka", "Kowalskeiego"));
+        List<Student>studenci1=new ArrayList<>();
+        studenci1.add(new Student("Jakubowski", "Kowalski"));
+        studenci1.add(new Student("Janusz", "Chybaty"));
+        studenci1.add(new Student("Grazynka", "Kowalskeiego"));
 
         Map< Integer, List<Student> > grupy = new HashMap<>();
         grupy.put(32,studenci);
@@ -42,29 +42,20 @@ public class Main {
         System.out.println(t1 + "<----- Nauczyciel od Programowania");
 
         List<Product> produkciki = new ArrayList<>();
-        Map<Integer,Product> mapa_productow =new HashMap<>();
-        Map<String,List<Product>> mapa_productow1 =new HashMap<>();
-
-        List<Product>slodycze=new ArrayList<>();
-        List<Product>narzedzia=new ArrayList<>();
-        List<Product>oprogramowanie=new ArrayList<>();
-        List<Product>inne=new ArrayList<>();
-        List<Product>nowa_kategoria=new ArrayList<>();
-        String zmienna_kategorii="";
-
+        Map<Integer,Product> mapaProductowId =new HashMap<>();
+        Map<String,List<Product>> mapaProductowCategory =new HashMap<>();
 
         try(BufferedReader in = new BufferedReader(new FileReader("produkty.txt"))) {
 
             String s = in.readLine();
 
 
-
             while(s!=null) {
                 String[] pola = s.split(";");
-               // System.out.println(pola[0]);=====ID
-              //  System.out.println(pola[1]);=====Nazwa
-               // System.out.println(pola[2]);=====categoria
-               // System.out.println(pola[3]);=====Cena
+                // System.out.println(pola[0]);=====ID
+                //  System.out.println(pola[1]);=====Nazwa
+                // System.out.println(pola[2]);=====categoria
+                // System.out.println(pola[3]);=====Cena
 
                 double c = Double.parseDouble(pola[3]);
                 Integer a = Integer.parseInt(pola[0]);
@@ -72,59 +63,32 @@ public class Main {
                 Product p1 =new Product(a,pola[1],c,pola[2]);
 
                 produkciki.add(p1);
-                mapa_productow.put(a,p1);
 
-                switch (pola[2])
-                {
-                    case "słodycze":
-                        slodycze.add(p1);
-                        break;
-                    case "narzędzia":
-                        narzedzia.add(p1);
-                        break;
-                    case "oprogramowanie":
-                        oprogramowanie.add(p1);
-                        break;
-                    case "inne":
-                        inne.add(p1);
-                        break;
-                        default:
-                            nowa_kategoria.add(p1);
-                            zmienna_kategorii=pola[2];
+                mapaProductowId.put(a, p1);
 
 
-                }
                 s = in.readLine();
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
-       // System.out.println(produkciki);
-        Product p1 =mapa_productow.get(103);
-        System.out.println("  ");
-        System.out.println(p1+ "<------Produkt o indexie 103");
-        System.out.println("  ");
+        for(Product p:produkciki) {
 
-        Map<String,List<Product>> mapa_categorii =new HashMap<>();
-        mapa_categorii.put("słodycze",slodycze);
-        mapa_categorii.put("narzędzia",narzedzia);
-        mapa_categorii.put("oprogramowanie",oprogramowanie);
-        mapa_categorii.put("inne",inne);
+            String cat=p.getCategory();
+            if(!mapaProductowCategory.containsKey(cat)) {
 
-        if(zmienna_kategorii !="")
-        {
-            mapa_categorii.put(zmienna_kategorii,nowa_kategoria);
-            List<Product> s3 =mapa_categorii.get(zmienna_kategorii);
-            System.out.println(s3+" <------To jest kategoria "+zmienna_kategorii);
+                mapaProductowCategory.put(cat,new ArrayList());
+            }
+
+            mapaProductowCategory.get(cat).add(p);
+
         }
 
-
-        List<Product> s1 =mapa_categorii.get("słodycze");
-        System.out.println(s1+" <------To sa słodycze");
-
-        List<Product> s2 =mapa_categorii.get("inne");
-        System.out.println(s2+" <------To sa inne");
+        System.out.println("Wyszukiwanie za pomocą id");
+        System.out.println(mapaProductowId.get(103));
+        System.out.println("Wyszukiwanie za pomocą kategorii");
+        System.out.println(mapaProductowCategory.get("słodycze"));
 
 
     }
